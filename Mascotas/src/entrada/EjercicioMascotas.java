f45920ab1e6fa865bd6b9806e33828b23f2db6a7
FP <FP@DESKTOP-5LVH4IU>
2020-10-14 11:12:23 +0200

    .

---
+++ Mascotas/src/entrada/EjercicioMascotas.java
@@ -0,0 +1,13 @@
+package entrada;
+
+public class EjercicioMascotas {
+
+	public static void main(String[] args) {
+	
+		
+		
+		
+
+	}
+
+}
--- Mascotas/src/negocio/ProcesamientoFicheroXML.java
+++ Mascotas/src/negocio/ProcesamientoFicheroXML.java
@@ -1,5 +1,27 @@
 package negocio;
 
-public class ProcesamientoFicheroXML {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.parsers.*;
+
+import modelo.Mascota;
+
+public class ProcesamientoFicheroXML {
+	
+	public List<Mascota> leerMascotas (String ruta){
+	
+		
+	List<Mascota> mascotas = new ArrayList<Mascota>();
+	
+	// clases DOM para la lectura del fichero XML
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	
+	
+	
+	
+	
+	return mascotas;
+	
+	}
 }
